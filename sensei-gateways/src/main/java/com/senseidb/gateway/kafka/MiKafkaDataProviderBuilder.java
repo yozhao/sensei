8aeb8aa2500f1d3fc88525b8c7635220a2064535
zhaoyonghui@gmail.com <zhaoyonghui@gmail.com>
2012-12-27 16:21:39 +0800

    Support kafka rewinding

---
+++ sensei-gateways/src/main/java/com/senseidb/gateway/kafka/MiKafkaDataProviderBuilder.java
@@ -0,0 +1,91 @@
+package com.senseidb.gateway.kafka;
+
+import java.io.Serializable;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.json.JSONObject;
+
+import proj.zoie.impl.indexing.StreamDataProvider;
+
+import com.senseidb.gateway.SenseiGateway;
+import com.senseidb.indexing.DataSourceFilter;
+import com.senseidb.indexing.ShardingStrategy;
+
+public class MiKafkaDataProviderBuilder extends SenseiGateway<DataPacket>{
+
+	private final Comparator<String> _versionComparator = new MiVersionComparator(); 
+
+	@Override
+  public StreamDataProvider<JSONObject> buildDataProvider(DataSourceFilter<DataPacket> dataFilter,
+      String oldSinceKey,
+      ShardingStrategy shardingStrategy,
+      Set<Integer> partitions) throws Exception
+  {
+	  String zookeeperUrl = config.get("kafka.zookeeperUrl");
+	  String consumerGroupId = config.get("kafka.consumerGroupId");
+    String topic = config.get("kafka.topic");
+    String timeoutStr = config.get("kafka.timeout");
+    String rewindStr = config.get("kafka.rewind");
+    int timeout = timeoutStr != null ? Integer.parseInt(timeoutStr) : 10000;
+    int batchsize = Integer.parseInt(config.get("kafka.batchsize"));
+    Boolean rewind = rewindStr != null ? Boolean.parseBoolean(rewindStr) : false;
+
+    if (dataFilter==null){
+      String type = config.get("kafka.msg.type");
+      if (type == null){
+        type = "json";
+      }
+    
+      if ("json".equals(type)){
+        dataFilter = new DefaultJsonDataSourceFilter();
+      }
+      else if ("avro".equals(type)){
+        String msgClsString = config.get("kafka.msg.avro.class");
+        String dataMapperClassString = config.get("kafka.msg.avro.datamapper");
+        Class cls = Class.forName(msgClsString);
+        Class dataMapperClass = Class.forName(dataMapperClassString);
+        DataSourceFilter dataMapper = (DataSourceFilter)dataMapperClass.newInstance();
+        dataFilter = new AvroDataSourceFilter(cls, dataMapper);
+      }
+      else{
+        throw new IllegalArgumentException("invalid msg type: "+type);
+      }
+    }
+    
+		MiKafkaStreamDataProvider provider = new MiKafkaStreamDataProvider(_versionComparator,zookeeperUrl,timeout,batchsize,consumerGroupId,topic,oldSinceKey,dataFilter, rewind);
+		return provider;
+	}
+
+  @Override
+  public Comparator<String> getVersionComparator() {
+    return _versionComparator;
+  }
+  
+  public static class MiVersionComparator implements Comparator<String>, Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final Pattern _numPattern = Pattern.compile("[0-9]+");
+
+    public int compare(String version1, String version2) {
+      if (version1 == version2) return 0;
+      if (version1 == null) return -1;
+      if (version2 == null) return 1;
+
+      String timestamp1 = version1.split(";")[0];
+      String timestamp2 = version2.split(";")[0];
+
+      if (_numPattern.matcher(timestamp1).matches() && _numPattern.matcher(timestamp2).matches()) {
+        try {
+          return Long.valueOf(timestamp1).compareTo(Long.valueOf(timestamp2));
+        } catch (Throwable t) {
+        }
+      }
+      return timestamp1.compareTo(timestamp2);
+    }
+
+    public boolean equals(String s1, String s2) {
+      return (compare(s1, s2) == 0);
+    }
+  }
+}
---
+++ sensei-gateways/src/main/java/com/senseidb/gateway/kafka/MiKafkaStreamDataProvider.java
@@ -0,0 +1,183 @@
+package com.senseidb.gateway.kafka;
+
+import java.nio.ByteBuffer;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeMap;
+
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.message.Message;
+import kafka.message.MessageAndMetadata;
+
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
+
+import proj.zoie.api.DataConsumer.DataEvent;
+import proj.zoie.impl.indexing.StreamDataProvider;
+
+import com.senseidb.indexing.DataSourceFilter;
+
+public class MiKafkaStreamDataProvider extends StreamDataProvider<JSONObject>{
+
+  private final String _topic;
+  private final String _consumerGroupId;
+  private ConsumerConnector _consumerConnector;
+  private ConsumerIterator<Message> _consumerIterator;
+
+  
+  private static Logger logger = Logger.getLogger(KafkaStreamDataProvider.class);
+  private final String _zookeeperUrl;
+  private final String _oldSinceKey;
+  private final Boolean _rewind;
+  private final int _kafkaSoTimeout;
+  private volatile boolean _started = false;
+  private final DataSourceFilter<DataPacket> _dataConverter;
+  private volatile Map<String, Long> _partitionOffsetMap = new TreeMap<String, Long>();
+
+  public MiKafkaStreamDataProvider(Comparator<String> versionComparator,String zookeeperUrl,int soTimeout,int batchSize,
+                                 String consumerGroupId,String topic, String oldSinceKey,
+                                 DataSourceFilter<DataPacket> dataConverter, Boolean rewind){
+    super(versionComparator);
+    _consumerGroupId = consumerGroupId;
+    _topic = topic;
+    super.setBatchSize(batchSize);
+    _zookeeperUrl = zookeeperUrl;
+    _oldSinceKey = oldSinceKey;
+    _rewind = rewind;
+    _kafkaSoTimeout = soTimeout;
+    _consumerConnector = null;    
+    _consumerIterator = null;
+
+    _dataConverter = dataConverter;
+    if (_dataConverter == null){
+      throw new IllegalArgumentException("kafka data converter is null");
+    }
+  }
+  
+  @Override
+  public void setStartingOffset(String version){
+  }
+  
+  @Override
+  public DataEvent<JSONObject> next() {
+    if (!_started) return null;
+
+    try
+    {
+      if (!_consumerIterator.hasNext())
+        return null;
+    }
+    catch (Exception e)
+    {
+      // Most likely timeout exception - ok to ignore
+      return null;
+    }
+
+    MessageAndMetadata<Message> messageAndMetadata = _consumerIterator.next();
+    if (logger.isDebugEnabled()){
+      logger.debug("got new message: " + messageAndMetadata.message());
+    }
+    
+    JSONObject data;
+    try {
+      int size = messageAndMetadata.message().payloadSize();
+      ByteBuffer byteBuffer = messageAndMetadata.message().payload();
+      byte[] bytes = new byte[size];
+      byteBuffer.get(bytes,0,size);
+      data = _dataConverter.filter(new DataPacket(bytes,0,size));
+      
+      if (logger.isDebugEnabled()){
+        logger.debug("message converted: "+data);
+      }
+      _partitionOffsetMap.put(messageAndMetadata.partition(), messageAndMetadata.offset());
+
+      String version = String.valueOf(System.currentTimeMillis()); 
+      for (Map.Entry<String, Long> entry : _partitionOffsetMap.entrySet()) {
+        version += ";" + entry.getKey() + ":" + entry.getValue().toString();
+      }
+      return new DataEvent<JSONObject>(data, version);
+    } catch (Exception e) {
+      logger.error(e.getMessage(),e);
+      return null;
+    }
+  }
+
+  @Override
+  public void reset() {
+  }
+
+  @Override
+  public void start() {
+    Properties props = new Properties();
+    props.put("zk.connect", _zookeeperUrl);
+    //props.put("consumer.timeout.ms", _kafkaSoTimeout);
+    props.put("groupid", _consumerGroupId);
+    props.put("autocommit.enable", "true");
+
+    ConsumerConfig consumerConfig = new ConsumerConfig(props);
+    _consumerConnector = Consumer.createJavaConsumerConnector(consumerConfig);
+
+    if (_rewind && _oldSinceKey != null) {
+      // rewind to last checkpoint
+      String parts[] = _oldSinceKey.split(";");
+      // the first one is time stamp
+      if (parts.length > 1) {
+        boolean failed = false;
+
+        for (int i = 1; i < parts.length; ++i) {
+          String pair[] = parts[i].split(":");
+          if (pair.length != 2) {
+            failed = true;
+            break;
+          }
+          if (!pair[1].matches("[0-9]+")) {
+            failed = true;
+            break;
+          }
+          _partitionOffsetMap.put(pair[0], Long.valueOf(pair[1]));
+        }
+
+        if (failed) {
+          _partitionOffsetMap.clear();
+        } else
+        {
+          _consumerConnector.commitOffsets(_topic, _partitionOffsetMap);
+        }
+      }
+    }
+    
+    Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+    topicCountMap.put(_topic, 1);
+    Map<String, List<KafkaStream<Message>>> topicMessageStreams =
+        _consumerConnector.createMessageStreams(topicCountMap);
+    List<KafkaStream<Message>> streams = topicMessageStreams.get(_topic);
+    KafkaStream<Message> KafkaStream = streams.iterator().next();
+    _consumerIterator = KafkaStream.iterator();
+
+    super.start();
+    _started = true;
+  }
+
+  @Override
+  public void stop() {
+    _started = false;
+
+    try
+    {
+      if (_consumerConnector!=null){
+        _consumerConnector.shutdown();
+      }
+    }
+    finally
+    {
+      super.stop();
+    }
+  }  
+}
