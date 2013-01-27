6cbd6b9a58c42797299636ec5e5b6d6ec11fb0fd
norton <heeseok.yu@gmail.com>
2013-01-28 08:02:02 +0900

    bugfix start-sensei-node.sh some linux -like gentoo- use /bin/ifconfig/

--- bin/start-sensei-node.sh
+++ bin/start-sensei-node.sh
@@ -14,7 +14,15 @@ bin=`cd "$bin"; pwd`
 OS=`uname`
 IP="" # store IP
 case $OS in
-   Linux) IP=`/sbin/ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}' | head -n 1`;;
+   Linux) 
+        if [ -f /sbin/ifconfig ]
+        then
+            IP=`/sbin/ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}' | head -n 1`
+        else
+        # some linux use /bin/ifconfig (e.g. gentoo)
+            IP=`/bin/ifconfig | grep 'inet ' | grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $2}' | head -n 1`
+        fi
+        ;;
    FreeBSD|OpenBSD|Darwin) IP=`ifconfig | grep -E '^en[0-9]:' -A 4 | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}' | head -n 1` ;;
    SunOS) IP=`ifconfig -a | grep inet | grep -v '127.0.0.1' | awk '{ print $2} ' | head -n 1` ;;
    *) IP="Unknown";;
