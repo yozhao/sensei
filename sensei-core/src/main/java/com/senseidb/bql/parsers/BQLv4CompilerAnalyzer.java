451ec51b782e6462a1142a2d5afa38840feeaf28
Sam Harwell <sam@tunnelvisionlabs.com>
2014-03-06 10:15:25 -0600

    Move semantic checks to listener

--- sensei-core/src/main/java/com/senseidb/bql/parsers/BQLv4CompilerAnalyzer.java
+++ sensei-core/src/main/java/com/senseidb/bql/parsers/BQLv4CompilerAnalyzer.java
@@ -464,6 +464,42 @@ public class BQLv4CompilerAnalyzer extends BQLv4BaseListener {
 
     @Override
     public void exitSelect_stmt(BQLv4Parser.Select_stmtContext ctx) {
+        if (ctx.order_by_clause().size() > 1) {
+            throw new IllegalStateException("ORDER BY clause can only appear once.");
+        }
+
+        if (ctx.limit_clause().size() > 1) {
+            throw new IllegalStateException("LIMIT clause can only appear once.");
+        }
+
+        if (ctx.group_by_clause().size() > 1) {
+            throw new IllegalStateException("GROUP BY clause can only appear once.");
+        }
+
+        if (ctx.distinct_clause().size() > 1) {
+            throw new IllegalStateException("DISTINCT clause can only appear once.");
+        }
+
+        if (ctx.execute_clause().size() > 1) {
+            throw new IllegalStateException("EXECUTE clause can only appear once.");
+        }
+
+        if (ctx.browse_by_clause().size() > 1) {
+            throw new IllegalStateException("BROWSE BY clause can only appear once.");
+        }
+
+        if (ctx.fetching_stored_clause().size() > 1) {
+            throw new IllegalStateException("FETCHING STORED clause can only appear once.");
+        }
+
+        if (ctx.route_by_clause().size() > 1) {
+            throw new IllegalStateException("ROUTE BY clause can only appear once.");
+        }
+
+        if (ctx.relevance_model_clause().size() > 1) {
+            throw new IllegalStateException("USING RELEVANCE MODEL clause can only appear once.");
+        }
+
         JSONObject jsonObj = new FastJSONObject();
         JSONArray selections = new FastJSONArray();
         JSONObject filter = new FastJSONObject();
